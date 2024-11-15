52f0d235956d525c58e013501e291ec34955a366
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2024-11-15 17:35:11 +0200

    - create mappers for movieDetails - create FetchMovieFirstSectionDetailsUseCase.kt

---
+++ app/src/main/java/com/example/teldatask/data/mapper/ToMovieDetailsDomainModelMapper.kt
@@ -0,0 +1,20 @@
+package com.example.teldatask.data.mapper
+
+import com.example.teldatask.data.Constants.Companion.IMAGE_BASE_URL
+import com.example.teldatask.data.data_sources.remote.retrofit.datamodel.movie_details.MovieDetailsResponse
+import com.example.teldatask.domain.model.MovieDetailsDomainModel
+
+fun MovieDetailsResponse.toMovieDetailsDomainModel(): MovieDetailsDomainModel {
+    return MovieDetailsDomainModel(
+        id = this.id,
+        name = this.originalTitle,
+        posterPath = IMAGE_BASE_URL + this.posterPath,
+        releaseDate = this.releaseDate,
+        rating = this.voteAverage,
+        revenue = this.revenue.toString(),
+        status = this.status,
+        details = this.overview,
+        categories = this.genres,
+        isFavourite = false
+    )
+}
---
+++ app/src/main/java/com/example/teldatask/domain/model/MovieDetailsDomainModel.kt
@@ -0,0 +1,16 @@
+package com.example.teldatask.domain.model
+
+import com.example.teldatask.data.data_sources.remote.retrofit.datamodel.movie_details.Genre
+
+data class MovieDetailsDomainModel(
+    val id: Int,
+    val name: String,
+    val details: String,
+    val posterPath: String,
+    val categories: List<Genre>,
+    val rating: Double,
+    val releaseDate: String,
+    val revenue: String,
+    val status: String,
+    val isFavourite:Boolean
+)

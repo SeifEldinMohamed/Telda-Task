7be0e8e94e0e57414ff03f983567f55f9ebf01ab
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2024-11-15 17:27:54 +0200

    - add AnimateShimmerDetails.kt - add CircleButton.kt

---
+++ app/src/main/java/com/example/teldatask/presentation/screens/movie_details_screen/components/shimmer_loading/AnimateShimmerDetails.kt
@@ -0,0 +1,178 @@
+package com.example.teldatask.presentation.screens.movie_details_screen.components.shimmer_loading
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AnimateShimmerDetails() {
+    val shimmerColors = listOf(
+        Color.LightGray.copy(alpha = 0.6f),
+        Color.LightGray.copy(alpha = 0.2f),
+        Color.LightGray.copy(alpha = 0.6f),
+    )
+
+    val transition = rememberInfiniteTransition(label = "")
+    val translateAnim = transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1000f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 700,
+                easing = LinearEasing
+            ),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = ""
+    )
+
+    val brush = Brush.linearGradient(
+        colors = shimmerColors,
+        start = Offset.Zero,
+        end = Offset(x = translateAnim.value, y = translateAnim.value)
+    )
+
+    ShimmerIssueItem(brush = brush)
+
+}
+
+@Composable
+fun ShimmerIssueItem(brush: Brush) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+               Spacer(
+                   modifier = Modifier
+                       .fillMaxWidth()
+                       .height(400.dp)
+                       .padding(top = 16.dp)
+                       .background(brush)
+               )
+
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(0.9f)
+                    .padding(vertical = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Spacer(
+                    modifier = Modifier
+                        .height(24.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                        .fillMaxWidth(fraction = 0.5f)
+                        .background(brush)
+                )
+
+                Spacer(
+                    modifier = Modifier
+                        .size(50.dp)
+                        .clip(CircleShape)
+                        .background(brush)
+                )
+            }
+
+            Spacer(
+                modifier = Modifier
+                    .height(140.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .fillMaxWidth(fraction = 0.9f)
+                    .background(brush)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Spacer(
+                modifier = Modifier
+                    .padding(start = 22.dp)
+                    .height(20.dp)
+                    .fillMaxWidth(fraction = 0.5f)
+                    .background(brush)
+                    .align(Alignment.Start)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Spacer(
+                modifier = Modifier
+                    .padding(start = 22.dp)
+                    .height(20.dp)
+                    .fillMaxWidth(fraction = 0.4f)
+                    .background(brush)
+                    .align(Alignment.Start)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Spacer(
+                modifier = Modifier
+                    .padding(start = 22.dp)
+                    .height(20.dp)
+                    .fillMaxWidth(fraction = 0.3f)
+                    .background(brush)
+                    .align(Alignment.Start)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            LazyRow(
+                Modifier.fillMaxWidth(0.9f)
+            ) {
+                items(3) {
+                    Spacer(
+                        modifier = Modifier
+                        .height(24.dp)
+                        .width(60.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(brush)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ShimmerItemPreview() {
+    ShimmerIssueItem(
+        brush = Brush.linearGradient(
+            listOf(
+                Color.LightGray.copy(alpha = 0.6f),
+                Color.LightGray.copy(alpha = 0.2f),
+                Color.LightGray.copy(alpha = 0.6f),
+            )
+        )
+    )
+}

205ce5fe9919258307a37a5e9ab85a6e996e3698
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2024-11-15 06:34:45 +0200

    - link viewModel in MoviesHomeScreen.kt

--- app/src/main/java/com/example/teldatask/presentation/MainActivity.kt
+++ app/src/main/java/com/example/teldatask/presentation/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
 import com.example.teldatask.presentation.navigation.AppNavHost
 import com.example.teldatask.presentation.ui.theme.TeldaTaskTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

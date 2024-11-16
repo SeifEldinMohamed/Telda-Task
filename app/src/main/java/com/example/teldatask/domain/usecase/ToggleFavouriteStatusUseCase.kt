70ba31ed8ecdfebb5013af5618e816346251d17e
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2024-11-16 10:46:42 +0200

    - add CheckFavouriteMovieUseCase.kt and ToggleFavouriteStatusUseCase.kt

---
+++ app/src/main/java/com/example/teldatask/domain/usecase/ToggleFavouriteStatusUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.teldatask.domain.usecase
+
+import com.example.teldatask.domain.model.MovieDomainModel
+import com.example.teldatask.domain.model.details.MovieDetailsDomainModel
+import com.example.teldatask.domain.repository.MovieDetailsRepository
+import com.example.teldatask.presentation.screens.movie_details_screen.model.MovieDetailsUiModel
+import javax.inject.Inject
+
+class ToggleFavouriteStatusUseCase @Inject constructor(
+    private val movieDetailsRepository: MovieDetailsRepository
+) {
+    suspend operator fun invoke(movie: MovieDetailsUiModel){
+        return movieDetailsRepository.toggleFavoriteStatus(movie)
+    }
+}
